c3feec771f3ce74adf02342500bb907214e0b0e7
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2024-03-24 22:42:26 -0400

    network training and testing -> 99%

--- README.md
+++ README.md
@@ -38,3 +38,35 @@ Results from using the NextJournal MNIST tutorial:
 `Test set: Avg loss: 0.1011, Accuracy: 9668/10000 (97%)` <- 3 epochs
 
 `Test set: Avg loss: 0.0595, Accuracy: 9822/10000 (98%)` <- 8 epochs
+
+### Main.py
+
+Used a combination of both the tutorials to download the data and save the test examples.
+
+Used [PyTorch documentation on building graphs](https://pytorch.org/docs/stable/nn.html) to set up network.
+
+Used [Tensorboard](https://pytorch.org/tutorials/intermediate/tensorboard_tutorial.html) to create network diagram.
+
+First test run (pre-training):
+
+> `Test set: Avg loss: 2.3078, Accuracy: 1078/10000 (11%)`
+
+Epoch 1:
+
+> `Test set: Avg loss: 0.1555, Accuracy: 9623/10000 (96%)`
+
+Epoch 2:
+
+> `Test set: Avg loss: 0.0969, Accuracy: 9759/10000 (98%)`
+
+Epoch 3:
+
+> `Test set: Avg loss: 0.0751, Accuracy: 9807/10000 (98%)`
+
+Epoch 4:
+
+> `Test set: Avg loss: 0.0649, Accuracy: 9842/10000 (98%)`
+
+Epoch 5:
+
+> `Test set: Avg loss: 0.0566, Accuracy: 9866/10000 (99%)`
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Kevin Heleodoro - MNIST digit recognition using CNN
 
 # ------ Import Statements ------  #
+
 import argparse  # Argument parser
 import torch  # PyTorch
 import torchvision  # Vision module
@@ -11,25 +12,53 @@ import matplotlib.pyplot as plt  # Plotting
 from torch.utils.data import DataLoader  # Data loader
 from torchvision import datasets  # Datasets
 from torchvision.transforms import ToTensor  # Transform to tensor
+from torch.utils.tensorboard import SummaryWriter  # TensorBoard
 
 # ------ Global Variables ------  #
 
+learning_rate = 0.01
+momentum = 0.5
+n_epochs = 5
 
 # ------ Class Definitions ------  #
 
 
-# Model for MNIST digit recognition
+# network for MNIST digit recognition
 class MyNetwork(nn.Module):
     # Constructor
     def __init__(self):
-        pass
+        super().__init__()
+        # Convolutional layer (10 5x5 filters)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # Convolutional layer (20 5x5 filters)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # Dropout layer (50%)
+        self.dropout = nn.Dropout(p=0.5)
+        # Fully connected layer (50 nodes)
+        self.fc1 = nn.Linear(in_features=320, out_features=50)
+        # Final fully connected layer (10 nodes)
+        self.fc2 = nn.Linear(in_features=50, out_features=10)
 
     # Forward pass for network
     def forward(self, x):
-        pass
+        # First convolutional layer, max pooling (2x2 window) and ReLU activation
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # Second convolutional layer, dropout, max pooling (2x2 window) and ReLU activation
+        x = F.relu(F.max_pool2d(self.dropout(self.conv2(x)), 2))
+        # Flatten tensor
+        x = x.view(-1, 320)
+        # x = x.view(-1, self.num_flat_features(x))
+        # Apply ReLU activation to first fully connected layer
+        x = F.relu(self.fc1(x))
+        # Apply second fully connected layer
+        x = self.fc2(x)
+        # Apply log softmax to output
+        return F.log_softmax(x, dim=1)
 
 
 # ------ Function Definitions ------  #
+
+
 # Print a border
 def print_border():
     print("\n" + "-" * 50 + "\n")
@@ -40,9 +69,33 @@ def arg_parser():
     # Creates the parser
     parser = argparse.ArgumentParser(description="MNIST digit recognition using CNN")
 
-    # Add arguments
+    # Download the MNIST dataset
     parser.add_argument(
         "--download", type=bool, default=True, help="Download the MNIST dataset"
+    )
+
+    # Save examples from the test data
+    parser.add_argument(
+        "--save_example",
+        type=bool,
+        default=False,
+        help="Save first 6 examples from the test data",
+    )
+
+    # Creates visualizer for the network
+    parser.add_argument(
+        "--visualize",
+        type=bool,
+        default=False,
+        help="Visualize the network using TensorBoard",
+    )
+
+    # Train the network
+    parser.add_argument(
+        "--train_network",
+        type=bool,
+        default=True,
+        help="Train the network using the MNIST dataset",
     )
 
     # Parse the arguments
@@ -117,7 +170,76 @@ def save_examples(test_dataloader):
     print(f"Examples saved successfully: {file_path}")
 
 
+# Train the network
+def train_network(
+    train_dataloader, network, optimizer, train_losses, train_counter, epoch
+):
+    print_border()
+    print("Training the network...")
+
+    # Set the network to training mode
+    network.train()
+
+    log_interval = 100
+
+    # Loop through the epochs
+    # for epoch in range(1, n_epochs + 1):
+    for batch_idx, (data, target) in enumerate(train_dataloader):
+        optimizer.zero_grad()  # Zero the gradients
+        output = network(data)  # Forward pass
+        loss = F.nll_loss(output, target)  # Calculate the loss
+        loss.backward()  # Backward pass
+        optimizer.step()  # Update the weights
+
+        if batch_idx % log_interval == 0:
+            print(
+                f"Training Epoch: {epoch} [{batch_idx * len(data)}/{len(train_dataloader.dataset)} ({100. * batch_idx / len(train_dataloader):.0f}%)]\tLoss: {loss.item():.6f}"
+            )
+            train_losses.append(loss.item())
+            train_counter.append(
+                (batch_idx * 64) + ((epoch - 1) * len(train_dataloader.dataset))
+            )
+            torch.save(network.state_dict(), "results/main/mnist_model.pth")
+            torch.save(optimizer.state_dict(), "results/main/mnist_optimizer.pth")
+
+    print("Training complete")
+
+
+# Test the network
+def test_network(test_dataloader, network, test_losses):
+    print_border()
+    print("Testing the network...")
+
+    # Set the network to evaluation mode
+    network.eval()
+
+    test_loss = 0
+    correct = 0
+
+    # No gradient calculation
+    with torch.no_grad():
+        for data, target in test_dataloader:
+            output = network(data)  # Forward pass
+            test_loss += F.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # Calculate the loss
+            pred = output.data.max(1, keepdim=True)[
+                1
+            ]  # Get the index of the max log-probability
+            correct += pred.eq(
+                target.data.view_as(pred)
+            ).sum()  # Calculate the number of correct predictions
+
+    test_loss /= len(test_dataloader.dataset)
+    test_losses.append(test_loss)
+    print(
+        f"\nTest set: Avg loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_dataloader.dataset)} ({100. * correct / len(test_dataloader.dataset):.2f}%)\n"
+    )
+
+
 # ------ Main Code ------  #
+
+
 def main():
     print_border()
     print("\nMNIST digit recognition using CNN\n")
@@ -130,7 +252,46 @@ def main():
         train_dataloader, test_dataloader = load_data()
 
     # Save examples from the test data
-    save_examples(test_dataloader)
+    if args.save_example:
+        save_examples(test_dataloader)
+
+    # Instantiate the network
+    network = MyNetwork()
+    optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+    print_border()
+    print("Network architecture:")
+    print(network)
+
+    # Visualize the network using TensorBoard
+    if args.visualize:
+        writer = SummaryWriter("results/main")
+        dataiter = iter(train_dataloader)
+        images, labels = next(dataiter)
+        writer.add_graph(network, images)
+        writer.close()
+
+    # Initialize the training and testing losses and counter
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i * len(train_dataloader.dataset) for i in range(n_epochs + 1)]
+
+    # Test the network pre-training
+    print(f"Running a pre-training test...")
+    test_network(test_dataloader, network, test_losses)
+
+    # Train and test the network
+    if args.train_network:
+        for epoch in range(1, n_epochs + 1):
+            train_network(
+                train_dataloader,
+                network,
+                optimizer,
+                train_losses,
+                train_counter,
+                epoch,
+            )
+            test_network(test_dataloader, network, test_losses)
 
     return
 
